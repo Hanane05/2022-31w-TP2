ae5b9f4bbcbce5320335189d13e6c5eebbac9700
Hanane Boukhalfa <e2195837@cmaisonneuve.qc.ca>
2022-08-21 00:47:59 -0400

    s12-c2 Ajouter element div avec la classe .blocs__horizontaux

--- front-page.php
+++ front-page.php
@@ -2,24 +2,25 @@
 get_header(); ?>
 <section class="site__main">
     <h1>Bienvenu!</h1>
-    <?php
-    if (have_posts()) :
-        while (have_posts()) : the_post(); ?>
-            <h2><a href="<?= get_permalink() ?>"><?php the_field('titre'); ?></a></h2>
-            <div class="image__article">
-                <?php
-                if (get_field('image')) : ?>
-                    <img src="<?php the_field('image'); ?>" />
+    <div class="blocs__horizontaux">
+        <?php
+
+        if (have_posts()) :
+            while (have_posts()) : the_post(); ?>
+                <article>
+                    <h2><a href="<?= get_permalink() ?>"><?php the_field('titre'); ?></a></h2>
+                    <!-- <div class="image__article"> -->
+                    <?php
+                    if (get_field('image')) : ?>
+                        <img src="<?php the_field('image'); ?>" />
+                    <?php endif; ?>
+                    <!-- </div> -->
                     <h3><?php the_field('sous_titre'); ?></h3>
                     <p><?php the_field('resume'); ?></p>
-                <?php endif; ?>
-            </div>
-
-
-
-
-        <?php endwhile; ?>
-    <?php endif; ?>
+                </article>
+            <?php endwhile; ?>
+        <?php endif; ?>
+    </div>
     <?php
     /* -------------------------------------- menu évènements */
     wp_nav_menu(array(
