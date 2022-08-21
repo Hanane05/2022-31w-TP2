3b449afeaf101350fdf426b0e69a7d281338e59a
Hanane Boukhalfa <e2195837@cmaisonneuve.qc.ca>
2022-08-21 18:52:04 -0400

    s12-c2 coleur de fond d'article

--- category-cours.php
+++ category-cours.php
@@ -1,24 +1,16 @@
-<?php
-get_header(); ?>
+<?php get_header(); ?>
+
 <section class="site__main">
-    <h1> Les cours</h1>
-    <?php
-    if (have_posts()) :
-        while (have_posts()) : the_post(); ?>
-            <h2><?php the_title(); ?></h2>
-
-            <?php $lien = " ... <a class='bouton' href='"
-                . get_permalink()
-                . "'>"
-                . substr(get_the_title(), 0, 8)
-                . "</a>";
-
-            ?>
-
-            <p><?= wp_trim_words(get_the_content(), 20, $lien) ?></p>
-
-
-        <?php endwhile; ?>
-    <?php endif; ?>
+    <h1>Liste de cours</h1>
+    <article class="grille__cours">
+        <input type="radio" name="modal" id="fermer">
+        <?php
+        if (have_posts()) :
+            while (have_posts()) : the_post();
+                get_template_part('content', 'cours');
+            endwhile; ?>
+        <?php endif; ?>
+    </article>
 </section>
+
 <?php get_footer(); ?>
